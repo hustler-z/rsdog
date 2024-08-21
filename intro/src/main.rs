c84f44f699966cd3d4798b7fb7d7164a3035ab99
Hustler-X <roxhustlin@gmail.com>
2024-08-21 18:18:28 +0800

    - Hustler 2024/08/21 WED -
    Rust: More notes

--- intro/src/main.rs
+++ intro/src/main.rs
@@ -64,12 +64,10 @@ use std::collections::HashMap;
  *
  * fn test_lifetime() {
  *	  let ref_0; -------------------------------+
- *                                              |
  *	  {                                         |
  *	      let num_0 = 2; ----+                  |
  *		  ref_0 = &num_0;    | 'a for num_0     | 'b for ref_0
  *	  } ---------------------+                  |
- *                                              |
  *	  println!("{}", ref_0);                    |
  * } -------------------------------------------+
  *
@@ -93,14 +91,28 @@ use std::collections::HashMap;
  * Anonymous Lifetime ??
  * '_
  *
- *
- * References
+ * --------------------------------------------------------------
  *
  * Rust has two types of pointers: safe pointers and raw pointers.
  *
  * References, &T, are primitives and the preferred pointer type
  * in Rust. References borrow values, means ownership remains
  * with the original binding.
+ *
+ * Raw pointers
+ * 1) Are allowed to ignore the borrowing rules by having both
+ *    immutable and mutable pointers or multiple mutable pointers
+ *    to the same location
+ * 2) Aren’t guaranteed to point to valid memory
+ * 3) Are allowed to be null
+ * 4) Don’t implement any automatic cleanup
+ *
+ * (a) *const T  -  immutable raw pointer
+ * (b) *mut T    -  mutable raw pointer
+ *
+ * Note the asterisk * not dereference operator. here in the
+ * context of raw pointers, immutable means that the pointer
+ * can't be directly assigned to after being dereferenced.
  *
  * The lifetime of the owner must outlive the borrower (i.e.,
  * the reference).
@@ -146,17 +158,17 @@ fn test_lifetime_label<'a>(ref_0: &'a u32, ref_1: &'a u32) -> &'a u32 {
 
 #[allow(unused)] /* attribute - unused function / variable */
 fn interact_console() {
-    let mut passwd = String::new();
+	let mut passwd = String::new();
 
-    println!("Enter the password:");
+	println!("Enter the password:");
 
-    io::stdin().read_line(&mut passwd).unwrap_or_default();
+	io::stdin().read_line(&mut passwd).unwrap_or_default();
 
-    if passwd != "" {
-        println!("\nPassword: {}", passwd.trim_end());
-    } else {
-        println!("\nEmpty password!!");
-    }
+	if passwd != "" {
+		println!("\nPassword: {}", passwd.trim_end());
+	} else {
+		println!("\nEmpty password!!");
+	}
 }
 
 /* --------------------------------------------------------------
@@ -241,7 +253,9 @@ fn check_ops(x: &u32) {
 	match x { // implicit dereference
 		2 => println!("step 2"),
 		5 => println!("step 5"),
-		_ => println!("step n"), // default
+		/* default，_ as wildcard pattern (忽略模式)
+		 */
+		_ => println!("step n"),
 	}
 }
 
@@ -271,6 +285,10 @@ fn check_ops_guard(pair: &(u32, u32)) {
 fn test_fp(x: u32) -> u32 {
 	x * x
 }
+
+/* Never Type
+ *
+ */
 
 // #[allow(unused)]
 
@@ -380,12 +398,19 @@ fn test_collections() {
 
 
 /* --------------------------------------------------------------
- * Macro - metaprogramming
+ * Macros
+ *
+ * macro_rule! - metaprogramming (元编程)
+ *
+ * Declarative macros
  *
  * The arguments of a macro are prefixed by $ and type
  * annotated with a designator:
  *
  * $ name : fragment-specifier
+ *
+ * Matches a Rust syntax fragment of the kind specified and
+ * binds it to the metavariable $name.
  *
  * 1)  block
  * 2)  expr
@@ -400,8 +425,27 @@ fn test_collections() {
  * 11) vis        - visibility qualifier
  * 12) meta       - an Attr, the contents of an attribute
  * 13) pat_param
+ *
+ * Repetition operators:
+ *
+ * 1) * — indicates any number of repetitions.
+ * 2) + — indicates any number but at least one.
+ * 3) ? — indicates an optional fragment with zero or one
+ *        occurrence.
  * --------------------------------------------------------------
  */
+macro_rules! vec_simple {
+	($( $x:expr ), *) => {
+		{
+			let mut tmp_vec = Vec::new();
+			$(
+				tmp_vec.push($x);
+			)*
+			tmp_vec
+		}
+	}
+}
+
 macro_rules! pr_debug {
 	($expression:expr) => {
 		println!("[Debug] {:?} = {:?}",
@@ -411,6 +455,8 @@ macro_rules! pr_debug {
 }
 
 /* --------------------------------------------------------------
+ *
+ * Generics (泛型编程)
  *
  * Generics are templates for building unique functions and type
  * definitions.
@@ -443,6 +489,27 @@ macro_rules! pr_debug {
  *   impl block  Trait name    Struct name
  *       \           |             /
  *     impl<T>   ATrait<T> for XStruct<T> {...}
+ *
+ * --------------------------------------------------------------
+ *
+ * Pattern (模式)
+ *
+ * Patterns come in two forms:
+ * a) refutable   (可反驳的模式)
+ * b) irrefutable (不可反驳的模式)
+ *
+ * Patterns that will match for any possible value passed are
+ * irrefutable.
+ *
+ * Pattern Syntax (模式语法)
+ *
+ * 1) Ignore an entire value with _
+ * 2) Ignore an unused variable by starting its name with _
+ * 3) Ignore remaining parts of a value with ..
+ * 4) Extra conditionals with match guards (匹配守卫)
+ * 5) @ bindings - create a variable that holds a value at the
+ *                 same time as testing that value for a pattern
+ *                 match. (绑定并验证模式匹配)
  *
  * --------------------------------------------------------------
  */
@@ -518,8 +585,8 @@ fn test_unsafety() {
 
 // --------------------------------------------------------------
 
-/* Closures are anonymous functions that save in a variable or
- * pass as arguments to other functions.
+/* Closures (闭包) are anonymous functions that save in a variable
+ * or pass as arguments to other functions.
  *
  * a.k.a lambda => e.g. |val| val + x
  *
@@ -557,7 +624,8 @@ mod hustler {
 /* --------------------------------------------------------------
  * Threads  - Native OS threads via the spawn function.
  *
- * Fearless Concurrency
+ * Fearless Concurrency (无畏并发)
+ *
  * (并发编程)
  * a) Concurrent programming - different parts of a program
  *                             execute independently.
@@ -565,9 +633,14 @@ mod hustler {
  * b) Parallel programming   - different parts of a program
  *                             execute at the same time.
  *
+ * Message passing (消息传递)
+ *
  * Channels - Communication between threads.
  *            Unidirectional flow of information allowed
  *            e.g. Sender -> Receiver
+ *
+ * Mutex<T> (互斥器)
+ * Arc<T> (原子引用计数)
  * --------------------------------------------------------------
  */
 use std::thread;
@@ -680,6 +753,21 @@ fn test_threads() {
 	}
 }
 
+/**
+ * Smart Pointer (智能指针)
+ *
+ * Using Box<T> to point to data on the heap
+ *
+ * Reference Counting (引用计数智能指针)
+ *
+ * The Rc<T> type keeps track of the number of references to
+ * a value to determine whether or not the value is still in
+ * use. If there are zero references to a value, the value
+ * can be cleaned up without any references becoming invalid.
+ *
+ * RefCell<T>
+ */
+
 // --------------------------------------------------------------
 
 fn main() {
@@ -692,11 +780,11 @@ fn main() {
 	 */
     let addr_var_1 = &var_1 as *const u32;
 
-    /* ----------------------------------------------------------
+	/* ----------------------------------------------------------
 	 * The two most used string types in Rust are:
 	 * a) String - stored as a vector of bytes (Vec<u8>), but
 	 *             guaranteed to always be a valid UTF-8
-     *             sequence.
+	 *             sequence.
 	 * b) &str   - a slice (&[u8]) that always points to a
 	 *             valid UTF-8 sequence, and can be used to
 	 *             view into a String.
@@ -704,25 +792,25 @@ fn main() {
 	 * &[T] is a view into Vec<T>
 	 *
 	 * String Type consists of three fields:
-     *
-     * +---------------+
-     * | backing array |
-     * +---------------+
-     * |    length     |
-     * +---------------+
-     * |   capacity    |
-     * +---------------+
-     *
-     * push
-     * push_str
-     * insert
-     * insert_str
-     *
-     * slices:
-     *
-     * string[starting index..ending index]
+	 *
+	 * +---------------+
+	 * | backing array |
+	 * +---------------+
+	 * |    length     |
+	 * +---------------+
+	 * |   capacity    |
+	 * +---------------+
+	 *
+	 * push
+	 * push_str
+	 * insert
+	 * insert_str
+	 *
+	 * slices:
+	 *
+	 * string[starting index..ending index]
 	 * ----------------------------------------------------------
-     */
+	 */
     let mut s_0 = String::from("Hustler");
     let s_1 = "Hacker".to_string();
     let mut s_2 = String::from("Fuk");
@@ -775,14 +863,14 @@ fn main() {
 
     println!("Type casting: {} -> {}", var_0, var_1);
 
-    /* ----------------------------------------------------------
+	/* ----------------------------------------------------------
 	 * Print Implementation
 	 *
 	 * (a) Public facing {}
-     * (b) Developer facing {:?}
-     * {:#?} adds a linefeed between each element displayed.
+	 * (b) Developer facing {:?}
+	 * {:#?} adds a linefeed between each element displayed.
 	 * ----------------------------------------------------------
-     */
+	 */
     println!("Address of var_1: {:?}", addr_var_1);
 
 	println!("---------------------------------");
@@ -802,6 +890,9 @@ fn main() {
 			 rock.0, rock.1, rock.2);
 
 	/* Binding values to names - @
+	 *
+	 * In match expressions, you can match multiple patterns
+	 * using the | syntax, which is the pattern or operator.
 	 */
 	if let a@(5|65|100) = value {
 		println!("Binding @: Match found {}", a);
@@ -855,6 +946,12 @@ fn main() {
  * all(ConfigurationPredicateList)
  * any(ConfigurationPredicateList)
  * not(ConfigurationPredicate)
+ *
+ * OOP
+ *
+ * State Pattern (状态模式) is an object-oriented design pattern.
+ * The states are represented by a set of state objects, and
+ * the value's behavior changes based on its state.
  */
 
 // --------------------------------------------------------------
